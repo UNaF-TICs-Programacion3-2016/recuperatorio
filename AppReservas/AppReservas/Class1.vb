ae1b08f624ebd6b0d1a891453632374361c5e521
Heber <hebercaballero@gmail.com>
2016-11-04 14:17:25 -0300

    Proyecto Inicial para el Recuperatorio.

---
+++ AppReservas/AppReservas/Class1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Reserva
+
+
+    Public Sub Guardar()
+
+    End Sub
+
+    Public Function ListarReservas() As DataTable
+
+    End Function
+
+    Public Function CantidadDias() As Byte
+
+    End Function
+
+    Public Function ValidarFechas() As Boolean
+
+    End Function
+
+End Class
